7285cc723637d0b97613baaa3e4d55b19fda98bf
Mateus Pavao <mateus_e_mateus@hotmail.com>
2021-11-30 10:39:52 +0100

    Props

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+
+import './styles.css';
+
+import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load.posts';
+
+
+
+export class Home extends Component {
+  state ={
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <h1>React Advanced </h1>
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
+
+export default Home;
