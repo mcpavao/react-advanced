759793ede47d271d99a953897a99913bbaf1d733
Mateus Pavao <mateus_e_mateus@hotmail.com>
2021-12-02 08:58:20 +0100

    loads per post pagination react

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,12 +4,17 @@ import './styles.css';
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load.posts';
+import { Button } from '../../components/Button';
+
 
 
 
 export class Home extends Component {
   state ={
-    posts: []
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 4
   };
 
   async componentDidMount() {
@@ -17,8 +22,27 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
     const postsAndPhotos = await loadPosts();
-    this.setState({ posts: postsAndPhotos });
+    this.setState({ 
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  }
+
+  loadMorePosts = () => {
+    const {
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state;
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
   }
 
   render() {
@@ -28,6 +52,10 @@ export class Home extends Component {
       <section className="container">
         <h1>React Advanced </h1>
         <Posts posts={posts} />
+        <Button 
+          text="Load more posts"
+          onClick={this.loadMorePosts}
+        />
       </section>
     );
   }
